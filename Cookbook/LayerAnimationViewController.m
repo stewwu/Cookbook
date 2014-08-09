be5deaf75df96ee47af11399ea17db464de258a2
Che-Ching Wu <stewwu@gmail.com>
2014-08-10 00:17:54 +0800

    change basic CAAnimation demo cases.

--- Cookbook/LayerAnimationViewController.m
+++ Cookbook/LayerAnimationViewController.m
@@ -8,10 +8,10 @@
 
 #import "LayerAnimationViewController.h"
 
-@interface LayerAnimationViewController () <UIPickerViewDataSource, UIPickerViewDelegate>
+@interface LayerAnimationViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic) IBOutlet UIView *square;
-@property (nonatomic) IBOutlet UIPickerView *animationTypePickter;
+@property (nonatomic) IBOutlet UITableView *animationTypePicker;
 @property (nonatomic) NSArray *animationKeys;
 
 @end
@@ -30,18 +30,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
-    self.animationKeys = @[@"pick animation type", @"position.x", @"position.y", @"cornerRadius"];
+    self.animationKeys = @[@"byValue", @"toValue", @"mantain final state at end of animation", @"autoreverses", @"cornerRadius", @"transform.translation", @"transform.rotation"];
     
-    self.animationTypePickter.dataSource = self;
-    self.animationTypePickter.delegate = self;
+    self.animationTypePicker.dataSource = self;
+    self.animationTypePicker.delegate = self;
 }
 
 - (void)dealloc
 {
-    self.animationTypePickter.delegate = nil;
-    self.animationTypePickter.dataSource = nil;
+    self.animationTypePicker.delegate = nil;
+    self.animationTypePicker.dataSource = nil;
 }
 
 - (void)didReceiveMemoryWarning
@@ -61,51 +60,59 @@
  }
  */
 
-#pragma mark - UIPickerViewDataSource
+#pragma mark - UITableViewDataSource
 
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView;
-{
-    return 1;
-}
-
-// returns the # of rows in each component..
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return [self.animationKeys count];
 }
 
-#pragma mark - UIPickerViewDelegate
-
-- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return self.animationKeys[row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    cell.textLabel.text = self.animationKeys[indexPath.row];
+    
+    return cell;
 }
 
-- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
 {
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:self.animationKeys[row]];
-    animation.duration = 0.5f;
-    switch (row) {
-        case 1:
-            animation.fromValue = @(self.square.layer.position.x);
-            animation.byValue = @(10.0f);
-            self.square.layer.position = CGPointMake(self.square.layer.position.x + 10.0f, self.square.layer.position.y);
-            break;
-            
-        case 2:
-            animation.byValue = @(10.0f);
-            break;
-            
-        case 3:
-            animation.duration = 1.0f;
-            animation.byValue = @(20.0f);
-            break;
-            
-        case 0: // by pass
-        default:
-            return;
+    CABasicAnimation *animation;
+    
+    // NOTE: CALayer Animatable Properties - https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/CoreAnimation_guide/AnimatableProperties/AnimatableProperties.html#//apple_ref/doc/uid/TP40004514-CH11-SW1
+    // NOTE: Setting Interpolation Values - https://developer.apple.com/library/mac/documentation/graphicsimaging/reference/CABasicAnimation_class/Introduction/Introduction.html#//apple_ref/doc/uid/TP40004496-CH1-SW4
+    if ([self.animationKeys[indexPath.row] isEqualToString:@"byValue"]) {
+        animation = [CABasicAnimation animationWithKeyPath:@"position.x"];
+        animation.byValue = @(10.0f);
+    } else if ([self.animationKeys[indexPath.row] isEqualToString:@"toValue"]) {
+        animation = [CABasicAnimation animationWithKeyPath:@"position.x"];
+        animation.toValue = @(self.square.layer.position.x + 10.0f);
+    } else if ([self.animationKeys[indexPath.row] isEqualToString:@"mantain final state at end of animation"]) {
+        animation = [CABasicAnimation animationWithKeyPath:@"position.x"];
+        animation.fromValue = @(self.square.layer.position.x);
+        animation.byValue = @(10.0f);
+        self.square.layer.position = CGPointMake(self.square.layer.position.x + 10.0f, self.square.layer.position.y);
+    } else if ([self.animationKeys[indexPath.row] isEqualToString:@"autoreverses"]) {
+        animation = [CABasicAnimation animationWithKeyPath:@"position"];
+        animation.toValue = [NSValue valueWithCGPoint:CGPointMake(self.square.layer.position.x + 20.0f, self.square.layer.position.y + 20.0f)];
+        animation.autoreverses = YES;
+    } else if ([self.animationKeys[indexPath.row] isEqualToString:@"cornerRadius"]) {
+        animation = [CABasicAnimation animationWithKeyPath:@"cornerRadius"];
+        animation.byValue = @(20.0f);
+    } else if ([self.animationKeys[indexPath.row] isEqualToString:@"transform.translation"]) {
+        animation = [CABasicAnimation animationWithKeyPath:@"transform"];
+        animation.fromValue = [NSValue valueWithCATransform3D:CATransform3DMakeTranslation(0.0f, 0.0f, 0.0f)];
+        animation.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeTranslation(10.0f, 0.0f, 0.0f)];
+        self.square.layer.transform = CATransform3DMakeTranslation(10.0f, 0.0f, 0.0f);
+    } else if ([self.animationKeys[indexPath.row] isEqualToString:@"transform.rotation"]) {
+        animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];;
+        animation.toValue = @(45.0f * M_PI);
     }
-    animation.timingFunction=[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+
+    animation.duration = 1.0f;
+    animation.timingFunction=[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
     
     [self.square.layer addAnimation:animation forKey:nil];
 }
