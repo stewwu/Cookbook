cbb44fb2b6decbcffb731fdcc4fb631edb8bc9c3
Che-Ching Wu <stewwu@gmail.com>
2016-06-07 09:16:21 +0800

    add keyboard type list view controller.

---
+++ Cookbook/TextInput/KeyboardTypeViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardTypeViewController.h
+//  Cookbook
+//
+//  Created by Ching on 2016/6/2.
+//  Copyright © 2016年 Che-Ching Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardTypeViewController : UIViewController
+
+@end
---
+++ Cookbook/TextInput/TextInputViewController.m
@@ -0,0 +1,46 @@
+//
+//  TextInputViewController.m
+//  Cookbook
+//
+//  Created by Ching on 2016/6/7.
+//  Copyright © 2016年 Che-Ching Wu. All rights reserved.
+//
+
+#import "TextInputViewController.h"
+
+@interface TextInputViewController ()
+
+@property (nonatomic) NSArray *cells;
+
+@end
+
+@implementation TextInputViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+
+    self.cells = @[@[@"Keyboard Type List", @"KeyboardTypeViewController"]];
+}
+
+#pragma mark - <UITableViewDataSource>
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.cells.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    cell.textLabel.text = self.cells[indexPath.row][0];
+    return cell;
+}
+
+#pragma mark - <UITableViewDelegate>
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UIViewController *viewController = [[NSClassFromString(self.cells[indexPath.row][1]) alloc] init];
+    [self.navigationController pushViewController:viewController animated:YES];
+}
+
+@end
